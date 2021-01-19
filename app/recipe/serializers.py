8e0dacac58f53aee557d55720c939fe640c2f666
Francisco Hermosillo <fhermosillo@hermescorp.io>
2021-01-19 13:26:42 -0600

    Added create ingredient feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Tag,Ingredient
+from core.models import Tag, Ingredient
 
 
 class TagSerializer(serializers.ModelSerializer):
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag,Ingredient
+from core.models import Tag, Ingredient
 
 from recipe import serializers
 
@@ -25,7 +25,8 @@ class TagViewSet(viewsets.GenericViewSet,
         serializer.save(user=self.request.user)    
 
 class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin):
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
     """Manage ingredients in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -35,3 +36,7 @@ class IngredientViewSet(viewsets.GenericViewSet,
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
+        serializer.save(user=self.request.user)
